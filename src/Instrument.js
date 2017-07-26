86d5083349ffee844434a8f674e05fafdd463ff9
John Shammas <john@johnshammas.com>
2017-07-26 14:53:44 -0400

    Instrument class

--- src/Instrument.js
+++ src/Instrument.js
@@ -1,164 +1,201 @@
+/* @flow */
 import request from "./request";
 import Sessions from "./Sessions";
 
+export type Quote = {
+	bid: number,
+	ask: number,
+};
+
+/**
+ * @class Instrument
+ * @description Use the constructor if you have a flat JSON object that you would like to access instance methods on.
+ * @param {object} data - See response at http://developer.drivewealth.com/docs/get_instrument
+ */
 export default class Instrument {
+	instrumentID: string;
+	name: string;
+	exchangeID: string;
+	instrumentTypeID: string;
+	orderSizeMax: number;
+	orderSizeMin: number;
+	orderSizeStep: number;
+	rateAsk: number;
+	rateBid: number;
+	ratePrecision: number;
+	symbol: string;
+	tradeStatus: string;
+	urlImage: string;
+	urlInvestor: string;
+	chaikinPgr: string;
+	priorClose: number;
+	marketState: number;
+	fundamentalDataModel: Object;
 
-    constructor(data) {
-        for (let key of [
-            "instrumentID",
-            "name",
-            "exchangeID",
-            "instrumentTypeID",
-            "orderSizeMax",
-            "orderSizeMin",
-            "orderSizeStep",
-            "rateAsk",
-            "rateBid",
-            "ratePrecision",
-            "symbol",
-            "tradeStatus",
-            "urlImage",
-            "urlInvestor",
-            "chaikinPgr",
-            "priorClose",
-            "marketState",
-            "fundamentalDataModel",
-        ]) {
-            this[key] = data[key];
-        }
-    }
+	constructor(data: Object) {
+		Object.assign(this, data);
+	}
 
-    static get TRADE_STATUSES() { return {
-        INACTIVE: "0",
-        ACTIVE: "1",
-        CLOSED: "2",
-    } }
+	/**
+	 * @name TRADE_STATUSES
+	 * @memberof Instrument
+	 * @constant
+	 * @property {string} INACTIVE
+	 * @property {string} ACTIVE
+	 * @property {string} CLOSED
+	 */
+	static TRADE_STATUSES: {[status: string]: string} = {
+		INACTIVE: "0",
+		ACTIVE: "1",
+		CLOSED: "2",
+	};
 
-    static get CHART_COMPRESSIONS() { return {
-        DAY: 0,
-        MINUTE_1: 1,
-        MINUTE_5: 4,
-        MINUTE_30: 8,
-        HOUR: 9,
-        WEEK: 10
-    } }
+	/**
+	 * @name CHART_COMPRESSIONS
+	 * @memberof Instrument
+	 * @constant
+	 * @property {number} DAY
+	 * @property {number} MINUTE_1
+	 * @property {number} MINUTE_5
+	 * @property {number} MINUTE_30
+	 * @property {number} HOUR
+	 * @property {number} WEEK
+	 */
+	static CHART_COMPRESSIONS: {[status: string]: number} = {
+		DAY: 0,
+		MINUTE_1: 1,
+		MINUTE_5: 4,
+		MINUTE_30: 8,
+		HOUR: 9,
+		WEEK: 10,
+	};
 
-    static getBySymbol(symbol, cb) {
-        let symbols = symbol;
-        if (typeof symbol === "string") symbols = [symbol];
+	/**
+	 * @static
+	 */
+	static getBySymbol(symbol: Array<string> | string): Promise<Array<Instrument> | Instrument> {
+		const symbols = typeof symbol === "string" ? [symbol] : symbol;
 
-        request({
-            method: "GET",
-            endpoint: `/instruments?symbol${symbols.length > 1 ? "s" : ""}=${symbols.join(',')}`,
-            sessionKey: Sessions.getAny(),
-        }, (data) => {
-            data = data.map(ins => new Instrument(ins));
-            cb && cb(null, typeof symbol === "string" ? data[0] : data);
-        }, err => cb && cb(err));
-    }
+		return request({
+			method: "GET",
+			endpoint: `/instruments?symbols=${symbols.join(",")}`,
+			sessionKey: Sessions.getAny(),
+		}).then(({ body }) => {
+			body = body.map(instrument => new Instrument(instrument));
+			return typeof symbol === "string" ? body[0] : body;
+		});
+	}
 
-    static getByID(id, includeFundamentals = true, cb) {
-        request({
-            method: "GET",
-            endpoint: `/instruments/${id}${includeFundamentals ? '?options=F' : ''}`,
-            sessionKey: Sessions.getAny(),
-        }, (data) => {
-            cb && cb(null, new Instrument(data));
-        }, err => cb && cb(err));
-    }
+	/**
+	 * @static
+	 */
+	static getByID(id: string, includeFundamentals: boolean = true): Promise<Instrument> {
+		return request({
+			method: "GET",
+			endpoint: `/instruments/${id}${includeFundamentals ? "?options=F" : ""}`,
+			sessionKey: Sessions.getAny(),
+		}).then(({ body }) => new Instrument(body));
+	}
 
-    static getAll(tradeStatus, cb) {
-        if (!cb) {
-            cb = tradeStatus;
-            tradeStatus = "-1";
-        }
+	/**
+	 * @static
+	 */
+	static getAll(tradeStatus?: string = "-1"): Promise<Array<Instrument>> {
+		return request({
+			method: "GET",
+			endpoint: `/instruments?tradeStatus=${tradeStatus}`,
+			sessionKey: Sessions.getAny(),
+		}).then(({ body }) => body.map(instrument => new Instrument(instrument)));
+	}
 
-        request({
-            method: "GET",
-            endpoint: `/instruments?tradeStatus=${tradeStatus}`,
-            sessionKey: Sessions.getAny(),
-        }, (data) => {
-            cb && cb(null, data.map(ins => new Instrument(ins)));
-        }, err => cb && cb(err));
-    }
+	/**
+	 * @static
+	 */
+	static search(criteria: { name?: string, symbol?: string }): Promise<Array<Instrument>> {
+		let queryString = "?";
+		if (criteria.symbol) queryString += `symbol=${criteria.symbol}&`;
+		if (criteria.name) queryString += `name=${criteria.name}&`;
 
-    static search(criteria, cb) {
-        let queryString = "?";
-        if (criteria.symbol) queryString += `symbol=${criteria.symbol}&`;
-        if (criteria.name) queryString += `name=${criteria.name}&`;
+		return request({
+			method: "GET",
+			endpoint: `/instruments${queryString}`,
+			sessionKey: Sessions.getAny(),
+		}).then(({ body }) => body.map(instrument => new Instrument(instrument)));
+	}
 
-        request({
-            method: "GET",
-            endpoint: `/instruments${queryString}`,
-            sessionKey: Sessions.getAny(),
-        }, (data) => {
-            cb && cb(null, data.map(ins => new Instrument(ins)));
-        }, err => cb && cb(err));
-    }
+	/**
+	 * @instance
+	 */
+	getQuote(): Promise<Quote> {
+		return Instrument.getQuote(this.symbol);
+	}
 
-    getQuote(cb) {
-        return Instrument.getQuote(this.symbol, cb);
-    }
+	/**
+	 * @static
+	 */
+	static getQuote(symbol: string | Instrument | Array<string> | Array<Instrument>): Promise<Quote | {[symbol: string]: Quote}> {
+		const symbols: Array<string> = (Array.isArray(symbol) ? symbol : [symbol])
+			.map(sym => sym instanceof Instrument ? sym.symbol : sym);
 
-    static getQuote(symbol, cb) {
-        let userPassedOneSymbol = false;
-        if (!Array.isArray(symbol)) {
-            userPassedOneSymbol = true;
-            symbol = [symbol];
-        }
-        symbol = symbol.map(sym => {
-            return sym instanceof Instrument ? sym.symbol : sym;
-        });
+		return request({
+			method: "GET",
+			endpoint: `/quotes?symbols=${symbols.join(",")}`,
+			sessionKey: Sessions.getAny(),
+			addlHeaders: {
+				Accept: "text/plain",
+			},
+		}).then(({ body }) => {
+			let obj = {};
+			body = body.split("|").slice(10);
+			for (const rawQuote of body) {
+				const parsedQuote = rawQuote.split(",");
+				obj[parsedQuote[0]] = {
+					bid: Number(parsedQuote[1]),
+					ask: Number(parsedQuote[2]),
+				};
+			}
+			if (!Array.isArray(symbol)) obj = obj[symbols[0]];
+			return obj;
+		});
+	}
 
-        request({
-            method: "GET",
-            endpoint: `/quotes?symbols=${symbol.join(",")}`,
-            sessionKey: Sessions.getAny(),
-            addlHeaders: {
-                "Accept": "text/plain"
-            }
-        }, (data) => {
-            let obj = {};
-            data = data.split("|").slice(10);
-            for (let rawQuote of data) {
-                const parsedQuote = rawQuote.split(",");
-                obj[parsedQuote[0]] = {
-                    bid: Number(parsedQuote[1]),
-                    ask: Number(parsedQuote[2]),
-                };
-            }
-            if (userPassedOneSymbol) obj = obj[symbol[0]];
-            cb && cb(null, obj);
-        }, err => cb && cb(err));
-    }
+	/**
+	 * @param {date} dateStart
+	 * @param {date} dateEnd
+	 */
+	/**
+	 * @param {number} tradingDays
+	 */
+	getChartData(compression: number) {
+		if (arguments.length === 3) {
+			return Instrument.getChartData(this.instrumentID, compression, arguments[1], arguments[2]);
+		} else {
+			return Instrument.getChartData(this.instrumentID, compression, arguments[1]);
+		}
+	}
 
-    getChartData() {
-        if (arguments.length === 4) {
-            return Instrument.getChartData(this.instrumentID, arguments[0], arguments[1], arguments[2], arguments[3]);
-        } else {
-            return Instrument.getChartData(this.instrumentID, arguments[0], arguments[1], arguments[2]);
-        }
-    }
+	/**
+	 * @param {date} dateStart
+	 * @param {date} dateEnd
+	 */
+	/**
+	 * @param {number} tradingDays
+	 */
+	static getChartData(instrumentID: string, compression: number): Promise<Array<string>> {
+		let timeParams;
+		if (arguments.length === 4) {
+			const dateStart = arguments[2].toISOString().replace(/\.\d{3}/, "");
+			const dateEnd = arguments[3].toISOString().replace(/\.\d{3}/, "");
+			timeParams = `dateStart=${dateStart}&dateEnd=${dateEnd}`;
+		} else {
+			timeParams = `tradingDays=${arguments[2]}`;
+		}
 
-    static getChartData(instrumentID, compression) {
-        let cb, timeParams;
-        if (arguments.length === 5) {
-            const dateStart = arguments[2].toISOString().replace(/\.\d{3}/, "");
-            const dateEnd   = arguments[3].toISOString().replace(/\.\d{3}/, "");
-            timeParams = `dateStart=${dateStart}&dateEnd=${dateEnd}`;
-            cb = arguments[4];
-        } else {
-            timeParams = `tradingDays=${arguments[2]}`;
-            cb = arguments[3];
-        }
-
-        request({
-            method: "GET",
-            endpoint: `/bars?instrumentID=${instrumentID}&compression=${compression}&${timeParams}`,
-            sessionKey: Sessions.getAny(),
-        }, (data) => {
-            cb && cb(null, data.data.split("|"));
-        }, err => cb && cb(err));
-    }
+		return request({
+			method: "GET",
+			endpoint: `/bars?instrumentID=${instrumentID}&compression=${compression}&${timeParams}`,
+			sessionKey: Sessions.getAny(),
+		}).then(({ body }) => body.data.split("|"));
+	}
 
 }
