a8bc5a6116510b6659003d69391dcf4487ee4941
John Shammas <john@johnshammas.com>
2017-07-26 16:00:52 -0400

    funding class

--- src/Funding.js
+++ src/Funding.js
@@ -2,71 +2,101 @@ import request from "./request";
 import Sessions from "./Sessions";
 import { Config, HOSTS } from "./Config";
 
+/**
+ * @class Funding
+ * @description Static class for retrieving funding information.
+ */
 export default class Funding {
+	/**
+	 * @name ALLOWED_TYPES
+	 * @memberof Funding
+	 * @constant
+	 * @property {string} DEPOSIT
+	 * @property {string} WITHDRAW
+	 * @property {string} DEPOSIT_AND_WITHDRAW
+	 */
+	static ALLOWED_TYPES = {
+		DEPOSIT: "DEPOSIT",
+		WITHDRAW: "WITHDRAW",
+		DEPOSIT_AND_WITHDRAW: "DEPOSIT_AND_WITHDRAW",
+	};
 
-    static get ALLOWED_TYPES() { return {
-        DEPOSIT: "DEPOSIT",
-        WITHDRAW: "WITHDRAW",
-        DEPOSIT_AND_WITHDRAW: "DEPOSIT_AND_WITHDRAW",
-    } }
+	/**
+	 * @param {object} options
+	 */
+	static getFundingMethods({
+		userID,
+		accountID,
+		language,
+		minAmount,
+		maxAmount,
+		amount,
+		filters = {},
+	}: {
+		userID: string,
+		accountID: string,
+		language?: string,
+		minAmount?: number,
+		maxAmount?: number,
+		amount?: number,
+		filters?: {
+			name: string,
+			currency: string,
+			country: string,
+			allowed: string,
+		},
+	} = {}): Promise<Array<Object>> {
+		if (amount && (minAmount || maxAmount)) {
+			throw new Error(`"amount" is not compatible with "minAmount" or "maxAmount."`);
+		}
 
-    static getFundingMethods({
-        userID,
-        accountID,
-        language,
-        minAmount,
-        maxAmount,
-        amount,
-        filters = {},
-    } = {}, cb) {
-        if (amount && (minAmount || maxAmount)) throw new Error(`"amount" is not compatible with "minAmount" or "maxAmount."`);
+		let queryString = `partner=${Config.appsPartnerID}&userID=${userID}&accountID=${accountID}`;
+		if (language) queryString += `&language=${language}`;
+		if (minAmount) queryString += `&minAmount=${minAmount}`;
+		if (maxAmount) queryString += `&maxAmount=${maxAmount}`;
+		if (amount) queryString += `&amount=${amount}`;
+		if (filters.name) queryString += `&filter[name]=${filters.name}`;
+		if (filters.country) queryString += `&filter[country]=${filters.country}`;
+		if (filters.currency) queryString += `&filter[currency]=${filters.currency}`;
+		if (filters.allowed) queryString += `&filter[allowed]=${filters.allowed}`;
 
-        let queryString = `partner=${Config.appsPartnerID}&userID=${userID}&accountID=${accountID}`;
-        if (language) queryString += `&language=${language}`;
-        if (minAmount) queryString += `&minAmount=${minAmount}`;
-        if (maxAmount) queryString += `&maxAmount=${maxAmount}`;
-        if (amount) queryString += `&amount=${amount}`;
-        if (filters.name) queryString += `&filter[name]=${filters.name}`;
-        if (filters.country) queryString += `&filter[country]=${filters.country}`;
-        if (filters.currency) queryString += `&filter[currency]=${filters.currency}`;
-        if (filters.allowed) queryString += `&filter[allowed]=${filters.allowed}`;
+		return request({
+			method: "GET",
+			host: HOSTS.APPS,
+			endpoint: `/funding/methods?${queryString}`,
+			sessionKey: Sessions.get(userID),
+		}).then(({ body }) => body);
+	}
 
-        request({
-            method: "GET",
-            host: HOSTS.APPS,
-            endpoint: `/funding/methods?${queryString}`,
-            sessionKey: Sessions.get(userID),
-        }, (data) => {
-            cb && cb(null, data);
-        }, err => cb && cb(err));
-    }
+	/**
+	 * @static
+	 */
+	static getPastDeposits(userID: string) {
+		return request({
+			endpoint: "/funding/status",
+			sessionKey: Sessions.get(userID),
+		}).then(({ body }) => body.data);
+	}
 
-    static getPastDeposits(userID, cb) {
-        request({
-            endpoint: `/funding/status`,
-            sessionKey: Sessions.get(userID),
-        }, (data) => {
-            cb && cb(null, data.data);
-        }, err => cb && cb(err));
-    }
-
-	static getPricing (userID, cb) {
+	/**
+	 * @static
+	 */
+	static getSubscriptionPlans(userID: string) {
 		return request({
 			endpoint: "/funding/ach/subscription-plans",
 			sessionKey: Sessions.get(userID),
-		}, data => {
-			const pricing = data.data.map(function (pricing) {
-				return [].concat(pricing)
-				.sort((x, y) => x.amount - y.amount)
-				.map(price => Object.assign(
-					{},
-					price,
-					{ amount: Number(price.amount / 100) },
-				));
-			})[0];
+		}).then(({ body }) => {
+			const pricing = body.data.map((pricing) =>
+				[].concat(pricing)
+					.sort((x, y) => x.amount - y.amount)
+					.map(price => Object.assign(
+						{},
+						price,
+						{ amount: Number(price.amount / 100) },
+					)),
+			)[0];
 
-			cb && cb(null, pricing);
-		}, err => cb && cb(err));
+			return pricing;
+		});
 	}
-
 }
