19800d6d40e1b583dc1fe7a82a3ccd5ed4e09ce5
Timur Uzel <timurt.uzel@gmail.com>
2017-07-27 14:30:43 -0400

    make sure the user has no credit cards after running credit card tests

--- test/user.test.js
+++ test/user.test.js
@@ -7,31 +7,39 @@ beforeAll(async () => {
 });
 
 describe("Credit cards", () => {
-	test("get a user's credit cards", async () => {
-		expect(await user.getCreditCards()).toBeDefined();
+	afterAll(async () => {
+		expect(await user.getCreditCards()).toEqual([]);
 	});
 
-	test("static get credit cards", async () => {
-		expect(await User.getCreditCards(user.userID)).toBeDefined();
+	describe("instance", () => {
+		test("get", async () => {
+			expect(await user.getCreditCards()).toBeDefined();
+		});
+
+		test("add", async () => {
+			expect(await user.addCreditCard("tok_visa")).toHaveProperty("cardID");
+		});
+
+		test("remove", async () => {
+			const [card] = await user.getCreditCards();
+
+			expect(await user.removeCreditCard(card.cardID)).toBeUndefined();
+		});
 	});
 
-	test("add and remove a user's credit card", async () => {
-		const { cardID } = await user.addCreditCard("tok_visa");
+	describe("static", () => {
+		test("get", async () => {
+			expect(await User.getCreditCards(user.userID)).toBeDefined();
+		});
 
-		expect(cardID).toBeDefined();
+		test("add", async () => {
+			expect(await User.addCreditCard(user.userID, "tok_visa")).toHaveProperty("cardID");
+		});
 
-		expect(await user.removeCreditCard(cardID)).toBeUndefined();
+		test("remove", async () => {
+			const [card] = await User.getCreditCards(user.userID);
+
+			expect(await User.removeCreditCard(user.userID, card.cardID)).toBeUndefined();
+		});
 	});
-
-	test("static add and remove credit card", async () => {
-		const { cardID } = await User.addCreditCard(user.userID, "tok_visa");
-
-		expect(cardID).toBeDefined();
-
-		expect(await User.removeCreditCard(user.userID, cardID)).toBeUndefined();
-	});
-
-	test("credit cards list should be empty", async () => {
-		expect(await user.getCreditCards()).toBe(expect.arrayContaining([]));
-	})
 });
